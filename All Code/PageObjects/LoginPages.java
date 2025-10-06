63990ae2fb83dc158908238f668a910fa69b15c5
SanjayKumarPandiyan <sanjaysuganya111@gmail.com>
2025-10-06 10:47:19 +0530

    added login page object

--- All Code/PageObjects/LoginPages.java
+++ All Code/PageObjects/LoginPages.java
@@ -1,4 +1,7 @@
 class LoginPages{
+    public static void main(String a) {
+        System.out.println("Welcome to Login Page");
+    }
     public static void main() {
         System.out.println("Welcome to Login Page");
     }
